8c6c0e2f02b5ebe754fb67c8c8a9cce7bbf8bb7b
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-01-28 23:11:39 +0100

    add styles to navbar, navbar links, logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './navbar.css';
 import  { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
   return (
