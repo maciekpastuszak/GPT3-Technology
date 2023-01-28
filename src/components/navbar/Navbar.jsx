468a6cd59a2f481878f445eca7714ec496b6c21e
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-01-28 23:03:00 +0100

    add navbar classnames

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './navbar.css';
+import  { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="gpt__navbar">
+      <div className="gpt__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
