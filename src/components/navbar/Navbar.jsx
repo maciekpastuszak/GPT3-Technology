a34e97344f313cec6b6af8c4b3a9b5f10544448f
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-01-30 09:08:04 +0100

    correct mistakes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './navbar.css';
-import  { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import  { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 
 const Menu = () => (
@@ -28,7 +28,7 @@ const Navbar = () => {
           <Menu />
         </div>
       </div>
-      <div className="gpt3__navabar-sign">
+      <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
