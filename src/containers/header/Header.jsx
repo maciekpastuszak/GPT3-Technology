79870d971fb2d7c01b293421c7e8ad4a991ac616
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-01-31 22:37:13 +0100

    add header content div with people

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
@@ -9,15 +11,20 @@ const Header = () => {
           Let's Build Somethung Amazing with GPT3 OpenAI
         </h1>
         <p>
-        Lorem ipsum dolor sit amet. Est aliquam tempore et vero facilis rem excepturi velit. A Quis tenetur eum tempore tenetur est molestias quae ex atque officiis non consequatur sapiente in atque cupiditate. Sed modi neque aut molestiae reprehenderit id sint galisum eum perferendis molestias id repellat voluptatem.
-
-Est dolores laudantium aut eaque laudantium id quas ducimus eum consequuntur quia. Sit omnis rerum sed deserunt dolores qui error dolore.
-
-Et dolor explicabo qui galisum illo sit nesciunt corporis ut natus possimus eos impedit sunt? Sit voluptatem nihil sit dolore tempore aut quia vero et numquam optio et dolorum alias et quis debitis?
+        Lorem ipsum dolor sit amet. Est aliquam tempore et vero facilis rem excepturi velit. 
+        A Quis tenetur eum tempore tenetur est molestias quae ex atque officiis non consequatur 
+        sapiente in atque cupiditate. Sed modi neque aut molestiae reprehenderit id sint galisum 
+        eum perferendis molestias id repellat voluptatem.Est dolores laudantium aut eaque laudantium 
+        id quas ducimus eum consequuntur quia. Sit omnis rerum sed deserunt dolores qui error dolore.
+        Et dolor explicabo qui galisum illo sit nesciunt corporis ut natus possimus eos impedit sunt? 
+        Sit voluptatem nihil sit dolore tempore aut quia vero et numquam optio et dolorum alias et quis debitis?
         </p>
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your e-mail address" />
           <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
         </div>
       </div>
     </div>
