996d476b415cba8c5415839ec1c5f155e398d9d6
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-01-31 09:45:33 +0100

    add basic header elements

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,24 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Somethung Amazing with GPT3 OpenAI
+        </h1>
+        <p>
+        Lorem ipsum dolor sit amet. Est aliquam tempore et vero facilis rem excepturi velit. A Quis tenetur eum tempore tenetur est molestias quae ex atque officiis non consequatur sapiente in atque cupiditate. Sed modi neque aut molestiae reprehenderit id sint galisum eum perferendis molestias id repellat voluptatem.
+
+Est dolores laudantium aut eaque laudantium id quas ducimus eum consequuntur quia. Sit omnis rerum sed deserunt dolores qui error dolore.
+
+Et dolor explicabo qui galisum illo sit nesciunt corporis ut natus possimus eos impedit sunt? Sit voluptatem nihil sit dolore tempore aut quia vero et numquam optio et dolorum alias et quis debitis?
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your e-mail address" />
+          <button type="button">Get Started</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
