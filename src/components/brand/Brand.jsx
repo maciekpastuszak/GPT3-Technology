6ffe93272683d7821295a3a2fbcfa9e1f0d52c90
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-02-02 09:31:18 +0100

    add brand imgs to the brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -5,7 +5,23 @@ import './brand.css';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
   )
 }
 
