78fa752c4aa882c13a20b013877ea925b92c807d
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-02-13 22:58:15 +0100

    add remaingin part of the Footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -16,7 +16,28 @@ const Footer = () => {
           <img src={gpt3Logo} alt="logo" />
           <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
         </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
       </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
+      </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+      <p>@2023 GPT-3. All rights reserved.</p>
+    </div>
     </div>
   )
 }
