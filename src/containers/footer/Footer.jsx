934d787f7c5762ea77a6b6a7d32c2f5c8c6ceb72
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-02-13 22:54:48 +0100

    add footer basic content

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request early access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3_ffoter-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
