345360d0016130c2851a5b91720e47f840ba8245
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-02-14 09:34:43 +0100

    bugfix

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         <p>Request early access</p>
       </div>
       <div className="gpt3__footer-links">
-        <div className="gpt3_ffoter-links_logo">
+        <div className="gpt3__footer-links_logo">
           <img src={gpt3Logo} alt="logo" />
           <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
         </div>
