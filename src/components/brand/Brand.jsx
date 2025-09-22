d5fa470ba9ef7dbf35354bc9d5a85e3900a67a73
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2025-09-22 09:49:13 +0200

    fix: update package.json to use react-scripts version 0.0.0; refactor Brand and Blog components for improved readability and structure

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import {google, slack, atlassian, dropbox, shopify} from './imports';
-import './brand.css';
-
+import React from "react";
+import {
+  google,
+  slack,
+  atlassian,
+  dropbox,
+  shopify,
+} from "./containers/index.js";
+import "./brand.css";
 
 const Brand = () => {
   return (
-    <div className='gpt3__brand section__padding'>
+    <div className="gpt3__brand section__padding">
       <div>
         <img src={google} alt="google" />
       </div>
@@ -22,7 +27,7 @@ const Brand = () => {
         <img src={shopify} alt="shopify" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Brand
+export default Brand;
